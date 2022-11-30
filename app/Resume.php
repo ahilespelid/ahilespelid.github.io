036822059bcb690af4bff66c465def7eabc91849
Ahi Les <ahilespelid@gmail.com>
2022-11-30 17:16:02 +0300

    gen file

--- app/Resume.php
+++ app/Resume.php
@@ -1,8 +1,9 @@
 <?php 
 namespace Abie;
 /// */ Юзаем PhpOffice 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx; /// */
+use PhpOffice\PhpSpreadsheet\Spreadsheet; use PhpOffice\PhpSpreadsheet\Style\Fill; use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing; /// */
 
 class Resume {
     public function __construct(
@@ -85,15 +86,19 @@ public function setHash():?string{
 public function getHash():?string{return (empty($this->hash)) ? $this->setHash() : $this->hash;}    
 public function write():bool{
     $spreadsheet = new Spreadsheet(); $sheet = $spreadsheet->createSheet(1);
-    foreach($this as $k => $v){
-        if(preg_match('#photo|hash|hashTime|birth|get#i',$k,$m)){pa($k);continue;}
-        $title[] = $k;
-        //$body[] = (is_string($v)) ? $v : (is_array(current($v)) ? implode(', ',$v[$k]) : implode(', ',$v));
-        //echo $k.'=>';pa($v);
-    }
-    $exel []= $title;
-    $exel []= array($this->name['f'], $this->age, $this->age);
-    $sheet->fromArray($exel, NULL, 'A1'); $spreadsheet->removeSheetByIndex(0); ///*/ $spreadsheet->setActiveSheetIndex(1); ///*/
+    foreach($this as $k => $v){$i=1;
+        if(preg_match('#photo|hash|hashTime|birth|get|file|lates#i',$k,$m)){continue;}
+        $exel []= array($i=>$k, ('name' == $k) ? str_replace("&nbsp;", ' ', $this->name['f']) : 
+                                (('age' == $k) ? $this->age : str_replace(',', '|', str_replace(['"', '{', '}', 'https:', 'http:','\/','/','\\'], '', json_encode($v))) ));
+    ++$i;}
+///*/ Фотография
+    file_put_contents($tPhotoFile = sys_get_temp_dir().DS.uniqid('pht').'.jpeg', base64_decode(explode(',', $this->photo)[1]),LOCK_EX);
+    $d = new Drawing(); $d->setName('PHOTO'); $d->setDescription('Ahi Les');
+    $d->setWidth(1); $d->setHeight(320); $d->setCoordinates('A1');
+    $d->setPath($tPhotoFile); $d->setWorksheet($sheet);
+///*/
+    $sheet->getStyle('F1:Z16')->applyFromArray(['font'=>['color'=>['rgb' => '255,50,50'],'bold' => true,'size' => 11],'fill'=>['fillType' => Fill::FILL_SOLID,'startColor' => ['rgb' => '50,100,100']]]); 
+    $sheet->fromArray($exel, NULL, 'F1'); $spreadsheet->removeSheetByIndex(0); ///*/ $spreadsheet->setActiveSheetIndex(1); ///*/
     return ((new Xlsx($spreadsheet))->save($this->file)) ? true : false;
     }    
 }
